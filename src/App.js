99e770706bf4103e20c72354828dca1f7cb2a17a
yellowsub12 <fantasm1337@gmail.com>
2022-03-04 00:16:51 -0500

    Login feature with name in nav bar. Issue #8 #10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
-import React from 'react';
+import {auth} from "./firebase";
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import { useStateValue } from './StateProvider';
 import Footer from './footer';
 import  {BrowserRouter as Router, Switch, Routes, Route}
 from "react-router-dom"
 import Login from './Login';
 
 function App() {
+  const [{},dispatch] = useStateValue();
+  useEffect(()=>{auth.onAuthStateChanged(authUser => {
+    console.log(authUser, " logged in!");
+
+    if (authUser){
+      dispatch({
+        type: 'SET_USER',
+        user: authUser
+      })
+
+    } else {
+      dispatch({
+        type: 'SET_USER',
+        user: null
+      })
+    }
+  })},[])
+  
   return (
 
     
@@ -23,7 +43,7 @@ function App() {
 
 
     <Route path="/checkout" element={[<Header/>, <Checkout />,<Footer />]} />
-    <Route path="/Login" element={[<Header/>, <Login />]} />
+    <Route path="/Login" element={[ <Login />]} />
 
 
     <Route path="/" element={[<Header/>,<Home />,<Footer />]} />
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,10 @@
+import firebase from "firebase/compat/app";
+
+
+import "firebase/compat/auth";
+
+import "firebase/compat/firestore";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
@@ -17,3 +24,10 @@ const firebaseConfig = {
     measurementId: "G-07454XDVWF"
   
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
